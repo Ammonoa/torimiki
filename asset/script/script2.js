f7c32cb9a1697f1e984e6fc9da8cf35137104c9d
Ammonoa <204640352+Ammonoa@users.noreply.github.com>
2025-07-05 15:57:53 +0900

    Update script2.js

--- asset/script/script2.js
+++ asset/script/script2.js
@@ -1,5 +1,5 @@
 const u=()=>{
   let d=new Date,h=d.getHours()+d.getMinutes()/60,w=d.getDay();
-  t1.textContent=w===1?'定休日':h<17||h>=23?'準備中':h<22.5?'営業時間中':'L.O.終了';
+  t1.textContent=w===1?'定休日':h<17||h>=23?'準備中':h<22.5?'営業時間内':'L.O.終了';
 };
 u(),setInterval(u,1e4);
